7bc3ac76cf6ea2cdfbc6da5dff08633520baf8b6
Ansar Ahmad <ahmadansar264@gmail.com>
2025-07-07 14:31:25 +0500

    Add user Login functionality

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,6 +3,21 @@ import ApiError from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import ApoResponse from "../utils/ApiResponse.js"
 
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "something Went wrong while generating Refresh and Access Token")
+    }
+}
 
 const registerUser = asyncHandler(async(req,res) =>{
     try{
@@ -44,11 +59,53 @@ const registerUser = asyncHandler(async(req,res) =>{
     
 })
 
-const loginUser = async(req,res) =>{
+const userLogin = asyncHandler(async (req, res) => {
+    try {
+        const { email, password } = req.body
+    
+        if (!email || !password) {
+            throw new ApiError(400, "All Fields are Required")
+        }
+    
+        const user = await User.findOne({ email }).select("+password")
+        if (!user) {
+            throw new ApiError(404, "This User Doesnot Exist")
+        }
+    
+        const isPasswordValid = await user.isPasswordCorrect(password)
+        if (!isPasswordValid) {
+            throw new ApiError(404, "Invalid User Credentials")
+        }
+    
+        const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
+    
+        const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+    
+        const options = {
+            httpOnly: true,
+            secure: true,
+        }
+    
+        return res.status(200)
+            .cookie("accessToken", accessToken, options)
+            .cookie("refreshToken", refreshToken, options)
+            .json(
+                new ApiResponse(
+                    200,
+                    {
+                        user: loggedInUser, accessToken, refreshToken
+                    },
+                    "User Logged in Successfully"
+                )
+            )
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while Logging in User", error.message)
+    }
 
-}
+})
+
 
 export {
     registerUser,
-    loginUser
+    userLogin
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import express from "express"
-import {registerUser, loginUser} from '../controllers/auth.controller.js';
+import {registerUser, userLogin } from '../controllers/auth.controller.js';
 
 const router = express.Router()
 
 
 router.route("/register").post(registerUser)
-router.router("login").post(loginUser)
+router.router("login").post(userLogin)
 
 
 export {router}
