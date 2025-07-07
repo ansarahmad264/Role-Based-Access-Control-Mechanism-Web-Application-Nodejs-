20f6fa50358c4894aa9a85b76872af025a791811
Ansar Ahmad <ahmadansar264@gmail.com>
2025-07-07 14:33:33 +0500

    Add Routes

--- src/app.js
+++ src/app.js
@@ -7,4 +7,8 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 
+import { router as userRouter} from "./routes/auth.routes";
+
+app.use("/api/v1/auth", userRouter)
+
 export {app}
