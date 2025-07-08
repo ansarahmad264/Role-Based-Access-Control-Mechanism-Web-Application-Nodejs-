72f8712d57bdaffced69d876dfa31df15ddff523
Ansar Ahmad <ahmadansar264@gmail.com>
2025-07-08 12:21:58 +0500

    add validation middleware in routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,14 @@
 import express from "express"
 import {registerUser, userLogin, userLogout } from '../controllers/auth.controller.js';
 import { verifyJWT } from "../middlewares/Auth.js";
+import { validate } from "../middlewares/validateRequest.js";
+import { loginSchema, registerSchema } from "../validators/auth.validator.js";
 
 const router = express.Router()
 
 
-router.route("/register").post(registerUser)
-router.route("/login").post(userLogin)
+router.route("/register").post(validate(registerSchema) ,registerUser)
+router.route("/login").post(validate(loginSchema) ,userLogin)
 router.route("/logout").post(verifyJWT, userLogout)
 
 
